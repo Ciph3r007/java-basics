dfa24d42fc90d3a664c5ef9b264d3d40a68b396b
Ciph3r007 <apu466@gmail.com>
2021-10-24 16:49:12 +0600

    Refactor files

--- oop/src/com/ciph3r/UIEvent.java
+++
@@ -1,7 +0,0 @@
-package com.ciph3r;
-
-public interface UIEvent {
-    public default void onClick(){
-        System.out.println("asd");
-    };
-}
--- oop/src/com/ciph3r/UIEvent2.java
+++
@@ -1,7 +0,0 @@
-package com.ciph3r.oop;
-
-public interface UIEvent2 {
-    public default void onClick(){
-        System.out.println("asdasda");
-    };
-}
